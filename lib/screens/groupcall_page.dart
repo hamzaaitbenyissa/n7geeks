1f3c03d7c325b9b88c9e1c509a949fd4379238c7
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-06-09 12:02:10 +0100

    change video call design

--- lib/screens/groupcall_page.dart
+++ lib/screens/groupcall_page.dart
@@ -1,4 +1,5 @@
 import 'package:agora_uikit/agora_uikit.dart';
+import 'package:chatnow/allConstants/all_constants.dart';
 import 'package:flutter/material.dart';
 
 class VideoCallPage extends StatefulWidget {
@@ -28,25 +29,44 @@ class _VideoCallPageState extends State<VideoCallPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Agora UIKit'),
-          centerTitle: true,
-        ),
-        body: SafeArea(
-          child: Stack(
-            children: [
-              AgoraVideoViewer(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('N7geeks Talk'),
+        centerTitle: true,
+        backgroundColor: AppColors.primarycolor,
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            AgoraVideoViewer(
                 client: client,
-                layoutType: Layout.floating,
-                enableHostControls: true, // Add this to enable host controls
-              ),
-              AgoraVideoButtons(
-                client: client,
-              ),
-            ],
-          ),
+                layoutType: Layout.grid,
+                enableHostControls: true,
+                showNumberOfUsers: true,
+                disabledVideoWidget: Container(
+                    color: AppColors.primarycolor,
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(" N7 Geeks ... 🤍",
+                            style: TextStyle(
+                                color: AppColors.white,
+                                fontSize: Sizes.dimen_24,
+                                fontWeight: FontWeight.bold)),
+                        SizedBox(height: 20),
+                        Text("WE BELIEVE IN YOU !! ✌",
+                            style: TextStyle(
+                                color: AppColors.white,
+                                fontSize: Sizes.dimen_24,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    )))), // Add this to enable host controls
+            AgoraVideoButtons(
+              client: client,
+            ),
+          ],
         ),
       ),
     );
